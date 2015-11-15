1d5adc76817061aef05183017cfc8d5e5b39258f
Rob Wilson <roobert@gmail.com>
2015-11-15 14:29:46 +0000

    move init script to app

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,10 +4,6 @@ class json_template_api::install (
   ensure_resource('package', ['thin', 'grape', 'rack', 'erubis', 'git'],
     {'provider' => 'gem'})
 
-  file { '/etc/init/json_template_api.conf':
-    source => 'puppet:///modules/json_template_api/init.conf',
-  }
-
   vcsrepo { '/opt/json_template_api':
     ensure   => latest,
     provider => git,
@@ -15,9 +11,9 @@ class json_template_api::install (
     source   => 'https://github.com/roobert/json_template_api.git',
   }
 
-  file { "/etc/thin${thin_version}/json_template_api.yml":
+  file { '/etc/init/json_template_api.conf':
     ensure => link,
-    target => '/opt/json_template_api/thin.yml',
+    target => '/opt/json_template_api/init.conf',
     notify => Service['json_template_api'],
   }
 }
