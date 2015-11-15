05824b5937fc6d01612546b60074d605849dec0e
Rob Wilson <roobert@gmail.com>
2015-11-15 13:17:06 +0000

    parameterize thin version

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,4 +1,6 @@
-class json_template_api::install {
+class json_template_api::install (
+  $thin_version = 2.1,
+) {
 
   ensure_packages(['thin'])
   ensure_resource('package', ['grape', 'rack', 'erubis', 'git'], {'provider' => 'gem'})
@@ -9,7 +11,7 @@ class json_template_api::install {
     source   => 'https://github.com/roobert/JSONTemplateAPI.git',
   }
 
-  file { '/etc/thin2.2/JSONTemplateAPI.yml':
+  file { "/etc/thin${thin_version}/JSONTemplateAPI.yml":
     ensure => link,
     target => '/opt/JSONTemplateAPI/thin.yml',
     notify => Service['thin'],
