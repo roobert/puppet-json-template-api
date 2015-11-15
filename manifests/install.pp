85dca6b404f0c8c13c53310edd18fa7b76cd8823
Rob Wilson <roobert@gmail.com>
2015-11-15 13:20:32 +0000

    add ensure

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,11 +1,13 @@
 class json_template_api::install (
   $thin_version = 2.1,
 ) {
-
   ensure_packages(['thin'])
-  ensure_resource('package', ['grape', 'rack', 'erubis', 'git'], {'provider' => 'gem'})
+
+  ensure_resource('package', ['grape', 'rack', 'erubis', 'git'],
+    {'provider' => 'gem'})
 
   vcsrepo { '/opt/JSONTemplateApi':
+    ensure   => latest,
     provider => git,
     branch   => 'master',
     source   => 'https://github.com/roobert/JSONTemplateAPI.git',
