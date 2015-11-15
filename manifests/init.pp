b4e43dcedb61b83e578f0942e3f93cfe0946c622
Rob Wilson <roobert@gmail.com>
2015-11-15 13:12:39 +0000

    re-add ordering

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
 class json_template_api {
   include json_template_api::install
   include json_template_api::service
+
+  Class['json_template_api::install'] ->
+  Class['json_template_api::service']
 }
